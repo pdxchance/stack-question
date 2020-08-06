bb5693312397170704491187d9a9bc71a94074e7
Deanne Chance <dchance@hireology.com>
2020-08-06 17:12:46 -0500

    Push bare bones project

---
+++ stack-question/stack-question/Controllers/QuestionViewController.swift
@@ -0,0 +1,19 @@
+//
+//  QuestionViewController.swift
+//  stack-question
+//
+//  Created by Deanne Chance on 8/6/20.
+//  Copyright © 2020 Deanne Chance. All rights reserved.
+//
+
+import UIKit
+
+class QuestionViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = .white
+    }
+
+}
