d84533dbacab8fc43d47b5673ca510a59e83ed17
Deanne Chance <dchance@hireology.com>
2020-08-07 16:12:44 -0500

    Rename something

--- stack-question/stack-question/Config/Api.swift
+++ stack-question/stack-question/Config/Api.swift
@@ -1,5 +1,5 @@
 //
-//  Config.swift
+//  Api.swift
 //  stack-question
 //
 //  Created by Deanne Chance on 8/7/20.
@@ -9,11 +9,14 @@
 import Foundation
 import Alamofire
 
+// not needed yet
 let API_KEY = "u225vZYSBzkUrYVt61gaqg(("
 
+// api has baked in filters you can adjust on the portal, this filter is a reduced payload
 let baseURL = "https://api.stackexchange.com//2.2"
 let searchFilter = "/search/advanced?site=stackoverflow&filter=!5-dmZUPQyv_-cPv(D30VgU)hf1C)YXwx*dvVom&accepted=true&answers=2"
 
+// simple API wrapper
 func requestGET(_ strURL: String, params: [String : Any]?, success:@escaping (Data) -> Void, failure:@escaping (Error) -> Void) {
     
     AF.request(strURL, parameters: params).validate().responseJSON { (responseObject) -> Void in
